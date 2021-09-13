4e50b65f34cdcaa133842a50f49f448a84e68ac8
Pankaj Bhadane <pankaj-kb>
2021-09-13 15:21:24 +0530

    Intermediate level donw

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const projectID = '73182750-e32b-4a45-ab80-5d9e60a5fcdf';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID='73182750-e32b-4a45-ab80-5d9e60a5fcdf'
-      userName='magma-master'
-      userSecret='123123'
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
