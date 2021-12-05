5c39dd9aacd09349fc9d53a4b2b0324130878f00
Pankaj Bhadane <pankaj-kb>
2021-12-06 01:59:02 +0530

    added new files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = process.env.REACT_APP_PROJECT_ID;
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('./src/sounds/new_msg.mp3').play()}
+    />
+  );
+};
+
+export default App;
